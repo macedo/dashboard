ff389a29ecaa07b76f21f0147635a50db233e5a9
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-09-30 17:30:12 -0300

    create, list and show videos features

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,27 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all
   end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:notice] = 'Video was successfully created.'
+      redirect_to video_path(@video)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:source)
+  end
 end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -4,4 +4,31 @@ class VideosControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:videos)
   end
+
+  test 'should get new' do
+    get :new
+    assert_template :new
+    assert_not_nil assigns(:video)
+  end
+
+  test 'should create new video' do
+    assert_difference('Video.count') do
+      post :create, video: { source: 'video.mp4' }
+    end
+    assert_redirected_to video_path(assigns(:video))
+    assert_equal 'Video was successfully created.', flash[:notice]
+  end
+
+  test 'should not create new video and render new' do
+    assert_no_difference('Video.count') do
+      post :create, video: { source: '' }
+    end
+
+    assert_template :new
+  end
+
+  test 'should get show' do
+    get :show, id: Video.last.id
+    assert_not_nil assigns(:video)
+  end
 end
